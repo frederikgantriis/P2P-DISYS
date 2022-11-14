cecc4d789dedd8ccf77a9ed03902deaf83f27a4e
frmo24042 <frmo24042@gmail.com>
2022-11-14 09:39:09 +0100

    added lamport timestamp

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"math"
 	"net"
 	"os"
 	"strconv"
@@ -13,15 +14,19 @@ import (
 	"google.golang.org/grpc"
 )
 
+var lamport int32
+
 func main() {
 	arg1, _ := strconv.ParseInt(os.Args[1], 10, 32)
 	ownPort := int32(arg1) + 5000
+	lamport = 0
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	p := &peer{
 		id:            ownPort,
+		lamport:       lamport,
 		amountOfPings: make(map[int32]int32),
 		clients:       make(map[int32]ping.PingClient),
 		ctx:           ctx,
@@ -68,6 +73,7 @@ func main() {
 type peer struct {
 	ping.UnimplementedPingServer
 	id            int32
+	lamport       int32
 	amountOfPings map[int32]int32
 	clients       map[int32]ping.PingClient
 	ctx           context.Context
@@ -82,7 +88,8 @@ func (p *peer) Ping(ctx context.Context, req *ping.Request) (*ping.Reply, error)
 }
 
 func (p *peer) sendPingToAll() {
-	request := &ping.Request{Id: p.id}
+	lamport++
+	request := &ping.Request{Id: p.id, lamport: p.lamport}
 	for id, client := range p.clients {
 		reply, err := client.Ping(p.ctx, request)
 		if err != nil {
@@ -90,4 +97,12 @@ func (p *peer) sendPingToAll() {
 		}
 		fmt.Printf("Got reply from id %v: %v\n", id, reply.Amount)
 	}
+}
+
+func setLamportTimestamp(incoming int) {
+	lamport = int32(math.Max(float64(lamport), float64(incoming)) + 1)
+}
+
+func criticalSection() {
+	fmt.Println("A Critical Hello World")
 }
